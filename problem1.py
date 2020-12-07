04758f8e52d844c1013a3470f8b084508200af4f
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2020-12-07 18:42:51 +0000

    Solved problem2.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,20 @@
+"""
+Name: problem1.py
 
+Purpose: To create a program that converts Celsius to Fahrenheit for the cousin to understand the climate in Canada.
+
+Author: Huang.K
+
+Date created: 7/12/2020
+"""
+
+print("   ---[(°C) to (°F) Converter]---")
+
+# Input the temperature in Celsius
+degrees_in_celsius = float(input("\nEnter the temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+degrees_in_fahrenheit = ((degrees_in_celsius * 9) / 5 + 32)
+
+# Output the temperature in Fahrenheit
+print("\nThe temperature is", degrees_in_fahrenheit, "°F")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,24 @@
+"""
+Name: problem2.py
 
+Purpose: A program that computes the number of fried chicken shared with students and Mr.Fabroa
+
+Author: Huang.K
+
+Date created: 7/12/2020
+"""
+
+print("\no-o-o[Chicken Leftovers Calculator]o-o-o")
+
+# Input the amount of students and the amount of chicken bought
+number_of_students = float(input("\nEnter number of students: "))
+fried_chicken = int(input("Enter amount of fried chicken bought: "))
+
+# Compute the amount of fried chicken for each students 
+# and how much fried chicken Mr.Fabroa gets
+chicken_per_student = fried_chicken / number_of_students
+leftovers_for_fabroa = fried_chicken % number_of_students
+
+# Output the amount of chicken per student and leftovers for Mr.Fabroa
+print("\n", chicken_per_student, "pieces of fried chicken per student.")
+print("\n", leftovers_for_fabroa, "pieces of fried chicken leftovers for Mr. Fabroa!")
